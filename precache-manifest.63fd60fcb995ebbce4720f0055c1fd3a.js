300abb67f8e7ee3d8806949f1a35d03e8341a40a
RamanSlinka <romuch1986@gmail.com>
2021-05-05 00:01:11 +0300

    Updates

---
+++ precache-manifest.63fd60fcb995ebbce4720f0055c1fd3a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff5a39b31b2aad663e7a502e3c032d3e",
+    "url": "/Homeworks_v2/index.html"
+  },
+  {
+    "revision": "b2cd23364285c2dc0144",
+    "url": "/Homeworks_v2/static/css/main.0fb50f78.chunk.css"
+  },
+  {
+    "revision": "fb44e276265a770775d2",
+    "url": "/Homeworks_v2/static/js/2.4cfb1308.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Homeworks_v2/static/js/2.4cfb1308.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2cd23364285c2dc0144",
+    "url": "/Homeworks_v2/static/js/main.4c84ac2a.chunk.js"
+  },
+  {
+    "revision": "da72b2f9b4ee264197ca",
+    "url": "/Homeworks_v2/static/js/runtime-main.3039bfdd.js"
+  }
+]);
