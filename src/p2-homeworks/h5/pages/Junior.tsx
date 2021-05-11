28dcf674401598d3451a6319b098b50a27c91820
RamanSlinka <romuch1986@gmail.com>
2021-05-11 19:14:49 +0300

    HW 8 finished

--- src/p2-homeworks/h5/pages/Junior.tsx
+++ src/p2-homeworks/h5/pages/Junior.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import HW7 from "../../h7/HW7";
+import s from './../HW5.module.css'
+import HW8 from "../../h8/HW8";
 
 
 function Junior() {
     return (
-        <div>
+        <div className={s.bgJunior}>
 
             <HW7/>
+            <HW8/>
 
         </div>
     )
--- src/p2-homeworks/h8/HW8.tsx
+++ src/p2-homeworks/h8/HW8.tsx
@@ -1,8 +1,14 @@
 import React, {useState} from 'react'
 import {homeWorkReducer} from './bll/homeWorkReducer'
 import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
+import s from './HW8.module.css'
 
-// export type UserType =
+export type UserType = {
+    _id: number
+    name: string
+    age: number
+
+}
 
 const initialPeople = [
     {_id: 0, name: 'Кот', age: 3},
@@ -14,33 +20,43 @@ const initialPeople = [
 ]
 
 function HW8() {
-    const [people, setPeople] = useState<any>(initialPeople) // need to fix any
+    const [people, setPeople] = useState<UserType[]>(initialPeople)
 
     // need to fix any
-    const finalPeople = people.map((p: any) => (
-        <div key={p._id}>
-            some name, age
+    const finalPeople = people.map((p: UserType) => (
+        <div key={p._id} className={s.item}>
+            <span>{p.name}</span>
+
+            {p.age}
         </div>
     ))
 
-    const sortUp = () => setPeople(homeWorkReducer(initialPeople, {type: 'sort', payload: 'up'}))
+    const sortUp = () => setPeople(homeWorkReducer(initialPeople,
+        {type: 'sort', payload: 'up'}))
+    const sortDown = () => setPeople(homeWorkReducer(initialPeople,
+        {type: 'sort', payload: 'down'}))
+    const chek18 = () => setPeople(homeWorkReducer(initialPeople,
+        {type: 'check', payload: 18}))
 
     return (
         <div>
             <hr/>
+            <hr/>
             homeworks 8
-
-            {/*should work (должно работать)*/}
+            <hr/>
             {finalPeople}
 
-            <div><SuperButton onClick={sortUp}>sort up</SuperButton></div>
-            <div>sort down</div>
-            check 18
+            <div>
+                <SuperButton onClick={sortUp} className={s.button}>sort up</SuperButton>
+                <SuperButton onClick={sortDown} className={s.button}>sort down</SuperButton>
+                <SuperButton onClick={chek18} className={s.button}>check 18</SuperButton>
+            </div>
+
 
             <hr/>
             {/*для личного творчества, могу проверить*/}
             {/*<AlternativePeople/>*/}
-            <hr/>
+
         </div>
     )
 }
